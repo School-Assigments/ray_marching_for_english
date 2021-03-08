1c35b2f69c2797535202700a192a806ba7c2419c
kotfind <kotfind@yandex.ru>
2021-03-08 18:37:11 +0300

    SDFs for sphere and box

--- main.cpp
+++ main.cpp
@@ -23,11 +23,20 @@ bool operator<(const map_val& lhs, const map_val& rhs) {
     return lhs.d < rhs.d;
 }
 
+float sdfsphere(const vec& p, const double& r) {
+  return len(p) - r;
+}
+
+double sdfbox(const vec& p, const vec& sz) {
+    vec q = vec{abs(p.x), abs(p.y), abs(p.z)} - sz;
+    return len({max(0., q.x), max(0., q.y), max(0., q.z)}) + min(0., max(q.x, max(q.y, q.z)));
+}
+
 map_val map(const vec& p) {
-    map_val d1 = {dist(p, vec(0, 0, 2)) - 1,
-                  {{1., 0., 0.}, 0.4, 0.9, 0.1, 100, 0.0}};
-    map_val d2 = {dist(p, vec(-2, -0.3, 3)) - 0.5,
-                  {{1., 1., 1.}, 0.0, 0.3, 1., 50, 0.7}};
+    static const material red = {{1., 0., 0.}, 0.4, 0.9, 0.1, 100, 0.0};
+    static const material mirror = {{1., 1., 1.}, 0.0, 0.3, 1., 50, 0.7};
+    const map_val d1 = {sdfbox(p - vec{0., 0., 8.}, {5., 5., 1.}), mirror};
+    const map_val d2 = {sdfsphere(p - vec{1., 0., -5.}, 2.), red};
     return min(d1, d2);
 }
 
