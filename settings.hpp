170269f34254f1b2e406a520974de09bfcecaaad
kotfind <kotfind@yandex.ru>
2021-03-15 21:37:55 +0300

    Change settings

--- settings.hpp
+++ settings.hpp
@@ -4,20 +4,20 @@
 #include "vec.hpp"
 
 namespace settings {
-    const int IMAGE_WIDTH  = 1920;
-    const int IMAGE_HEIGHT = 1280;
+    const int IMAGE_WIDTH  = 300;
+    const int IMAGE_HEIGHT = 300;
 
     namespace marching {
-        const double ZFAR  = 1e2;
-        const double ZNEAR = 1;
-        const int MAX_ITER = 1e3;
-        const double COLL_DIST = 0.0001;
+        const double ZFAR  = 20;
+        const double ZNEAR = 1.;
+        const int MAX_ITER = 1e4;
+        const double COLL_DIST = 1e-5;
         const int MAX_DEPTH = 5.;
     }
 
     namespace camera {
         const vec& target = {0., 0., 0.};
-        const vec& pos = {-3., 3., -3.};
+        const vec& pos = {2.5, -2.0, -1.5};
     }
 }
 
